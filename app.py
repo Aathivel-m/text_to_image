b3ea4d812e725b09b58f2a8b0b2b401ff93e97b5
Aathivel-m <velan040301@gmail.com>
2025-10-03 09:53:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from PIL import Image
 
 # Replace this with your Colab backend URL
 
-API_URL = "https://75e93ea2b912.ngrok-free.app/generate"
+API_URL = "https://0c5e99384a6d.ngrok-free.app/generate"
 
 
 st.set_page_config(
